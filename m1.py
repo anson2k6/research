761b8123514fb1193e0c3b315526a769d0523d10
JusTS2077 <justin.shijoy@gmail.com>
2026-02-19 08:18:21 +0530

    Fixed the graph

--- m1.py
+++ m1.py
@@ -9,14 +9,19 @@ ion = st.selectbox("Select Ion", ["Pb2+", "H2PO4-"])
 conc = st.slider("Ion concentration (µM)", 0, 50, 10)
 
 wavelength = np.linspace(400, 600, 200)
+
+#This is just a formula simplified into python code: I0​(λ)=100*e^(−0.01(λ−480)^2)
 base_intensity = np.exp(-0.01*(wavelength-480)**2)*100
 
 if ion == "Pb2+":
-    intensity = base_intensity * np.exp(-0.05*conc)   # quenching
+    intensity = base_intensity * np.exp(-0.05*conc)  # quenching
 else:
     intensity = base_intensity * (1 + 0.05*conc)      # enhancement
 
-plt.plot(wavelength, intensity)
-plt.xlabel("Wavelength (nm)")
-plt.ylabel("Fluorescence Intensity")
-st.pyplot(plt)
+fig,ax = plt.subplots()
+
+ax.plot(wavelength, intensity)
+ax.set_xlabel("Wavelength (nm)")
+ax.set_ylabel("Fluorescence Intensity")
+ax.set_ylim(0,400)
+st.pyplot(fig)
